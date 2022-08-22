ca37850d14659b1d3510e9c51c950f1be95e54d1
echatzief <echatzief97@gmail.com>
2022-08-22 23:18:33 +0300

    - done some changes at user's API and also added PUT /users/:id

--- backend/api/user.go
+++ backend/api/user.go
@@ -13,7 +13,8 @@ type ValidationError struct {
 	Error string `json:"error"`
 }
 
-func ValidateUser(user models.User) []*ValidationError {
+// validate user fields
+func validateUser(user models.User) []*ValidationError {
 	var errors []*ValidationError
 	var validate = validator.New()
 	err := validate.Struct(user)
@@ -54,39 +55,7 @@ func GetUsers(c *fiber.Ctx) error {
 	return c.Status(200).JSON(users)
 }
 
-// POST /users
-func CreateUser(c *fiber.Ctx) error {
-
-	// validate if body is given
-	user := new(models.User)
-	if err := c.BodyParser(user); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": err.Error(),
-		})
-	}
-
-	// validate user fields
-	errors := ValidateUser(*user)
-	if errors != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(errors)
-	}
-
-	// check if user exists with same email then throw an error
-	existingUser := new(models.User)
-	database.Database.Handler.Where(&models.User{}).Find(&existingUser, "email = ? or username = ?", user.Email, user.Username)
-	if existingUser.Id != 0 {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": "User exists with given username or email",
-		})
-	}
-
-	// create user
-	database.Database.Handler.Create(&user)
-
-	return c.JSON(user)
-}
-
-// GET /user
+// GET /user/:id
 func GetUser(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	var user models.User
@@ -104,9 +73,73 @@ func GetUser(c *fiber.Ctx) error {
 	return c.JSON(user)
 }
 
+// POST /users
+func CreateUser(c *fiber.Ctx) error {
+
+	// validate if body is given
+	user := new(models.User)
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	// validate user fields
+	errors := validateUser(*user)
+	if errors != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(errors)
+	}
+
+	// check if user exists with same email or username then throw an error
+	existingUser := new(models.User)
+	database.Database.Handler.Where(&models.User{}).Find(&existingUser, "email = ? or username = ?", user.Email, user.Username)
+	if existingUser.Id != 0 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "User exists with given username or email",
+		})
+	}
+
+	// create user
+	database.Database.Handler.Create(&user)
+
+	return c.JSON(user)
+}
+
 // PUT /user/:id
 func UpdateUser(c *fiber.Ctx) error {
-	return nil
+	id, err := c.ParamsInt("id")
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "Please ensure that id is an integer",
+		})
+	}
+
+	// validate if body is given
+	user := new(models.User)
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	// check if user that we want to update already exists
+	existingUser := new(models.User)
+	database.Database.Handler.Where(&models.User{}).Find(&existingUser, "id = ?", id)
+	if existingUser.Id == 0 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "User that you are trying to update doesn't exist",
+		})
+	}
+
+	// update user with new fields
+	database.Database.Handler.Model(&models.User{}).Where("id = ?", id).Updates(user)
+
+	// find user after update
+	user = new(models.User)
+	database.Database.Handler.Where(&models.User{}).Find(&user, "id = ?", id)
+
+	return c.JSON(user)
 }
 
 // DELETE /user/:id
